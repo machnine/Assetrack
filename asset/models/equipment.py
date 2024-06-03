16ae1d659dc5c00335016a2e13f3b9c964060029
Mian Chen <mianchen@gmail.com>
2024-06-03 13:17:03 +0100

    added inventory_number to equipment model

--- asset/models/equipment.py
+++ asset/models/equipment.py
@@ -7,13 +7,14 @@ class Equipment(models.Model):
     name = models.CharField(max_length=100)
     model_number = models.CharField(max_length=100)
     serial_number = models.CharField(max_length=100)
+    inventory_number = models.CharField(max_length=100, null=True, blank=True)
     porcurement_date = models.DateField(null=True, blank=True)
     commission_date = models.DateField(null=True, blank=True)
     warranty_period = models.IntegerField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    location = models.ForeignKey('Location', on_delete=models.PROTECT)
-    status = models.ForeignKey('Status', on_delete=models.PROTECT)
+    category = models.ForeignKey("Category", on_delete=models.PROTECT)
+    location = models.ForeignKey("Location", on_delete=models.PROTECT)
+    status = models.ForeignKey("Status", on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
