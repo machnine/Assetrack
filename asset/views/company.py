e81ca98b059e7e5beab6711f4290fa634076aa2f
Mian Chen <mianchen@gmail.com>
2024-06-07 14:15:16 +0100

    company search

--- asset/views/company.py
+++ asset/views/company.py
@@ -1,6 +1,7 @@
 """company views"""
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
@@ -14,6 +15,13 @@ class CompanyListView(LoginRequiredMixin, ListView):
     model = Company
     template_name = "asset/company_list.html"
     context_object_name = "companies"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(Q(name__icontains=query) | Q(notes__icontains=query))
+        return queryset.order_by("name")
 
 
 class CompanyCreateView(LoginRequiredMixin, CreateView):
@@ -40,9 +48,8 @@ class CompanyDeleteView(LoginRequiredMixin, DeleteView):
     model = Company
     template_name = "partials/object_delete.html"
     success_url = reverse_lazy("company_list")
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["cancel_url"] = reverse_lazy("company_list")
         return context
-
