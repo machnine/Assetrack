8803810abc4f055c65abd71fc6b70e1e489e7b47
Mian Chen <mianchen@gmail.com>
2024-06-19 18:08:03 +0100

    user password change view

--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,10 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path, reverse_lazy
 
-from .views import UserLoginView
+from .views import UserLoginView, UserPasswordChangeView
 
 urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page=reverse_lazy("login")), name="logout"),
+    path("password_change/", UserPasswordChangeView.as_view(), name="password_change"),
 ]
--- user/views.py
+++ user/views.py
@@ -2,10 +2,12 @@
 
 from django.contrib import messages
 from django.contrib.auth import get_user_model
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.html import format_html
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class UserLoginView(LoginView):
@@ -27,4 +29,25 @@ class UserLoginView(LoginView):
         user = get_user_model().objects.get(username=username)
         display_name = user.first_name or user.username
         messages.success(self.request, format_html(f"Welcome back, <b>{display_name}</b>."))
+        return super().form_valid(form)
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    """Custom password change form."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs.update({"class": "form-control"})
+
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    """Basic password change view."""
+
+    template_name = "user/password_change.html"
+    success_url = reverse_lazy("home")
+    form_class = UserPasswordChangeForm
+
+    def form_valid(self, form):
+        messages.success(self.request, "Your password has been changed.")
         return super().form_valid(form)
