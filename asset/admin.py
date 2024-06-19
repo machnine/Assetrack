b894d72e4d6b2003684426eb577df20a04fd3db8
Mian Chen <mianchen@gmail.com>
2024-06-19 15:52:33 +0100

    added calibration model to admin

--- asset/admin.py
+++ asset/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from asset.models import RecordType
+from asset.models import RecordType, Calibration
 
 admin.site.register(RecordType)
+admin.site.register(Calibration)
+
