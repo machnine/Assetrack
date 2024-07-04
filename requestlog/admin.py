3a7b26837ae4c4b833cf5de1c6ad5b692e92637c
Mian Chen <mianchen@gmail.com>
2024-07-04 12:37:59 +0100

    request log middleware

---
+++ requestlog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import RequestLog
+
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "method", "status_code", "ip_address", "path")
+    search_fields = ("ip_address", "method", "path")
+    list_filter = ("timestamp", "method", "status_code")
+
+    def get_queryset(self, request):
+        # Override get_queryset to handle empty querysets gracefully
+        queryset = super().get_queryset(request)
+        if not queryset.exists():
+            return RequestLog.objects.none()  # Return an empty queryset
+        return queryset
--- user/admin.py
+++ user/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "last_name", "first_name", "email", "is_staff", "is_active")
+    search_fields = ("username", "last_name", "first_name", "email")
+    list_filter = ("is_staff", "is_active")
