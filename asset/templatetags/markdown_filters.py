5ff7a84b7b4c3d99654adddd322899aa948b4a8c
Mian Chen <mianchen@gmail.com>
2024-08-06 21:22:42 +0100

    added markdown_filters for link

---
+++ asset/templatetags/markdown_filters.py
@@ -0,0 +1,26 @@
+"""implement markdown like syntax for text formatting"""
+
+import re
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+# Regular expression to match markdown links
+MARKDOWN_LINK_REGEX = r"\[([^\]]+)\]\(((?:http[s]?://)?[^\)]+)\)"
+
+
+@register.filter(name="md_link")
+def md_link(text):
+    """Convert markdown links to HTML links"""
+
+    def replace(match):
+        # extract the link text and URL from the match
+        link_text = match.group(1)
+        link_url = match.group(2)
+
+        return f"""<a href="{link_url}" target="_blank">{link_text}</a>"""
+
+    converted_text = re.sub(MARKDOWN_LINK_REGEX, replace, text)
+    return mark_safe(converted_text)
