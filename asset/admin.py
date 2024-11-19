95a2bb55e3a0be17a34f804e17a942771d4c4b64
Machnine <mianchen@gmail.com>
2024-11-19 15:45:45 +0000

    typo

--- asset/admin.py
+++ asset/admin.py
@@ -5,18 +5,6 @@ from django.contrib import admin
 from asset.models import Calibration, EquipmentType, License, MaintenanceRecordMenu, RecordType, SoftwareType, SiteConfiguration
 
 admin.site.register(Calibration)
-admin.site.register(License)
-admin.site.register(SoftwareType)
-
-admin.register(RecordType)
-class RecordTypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "color")
-
-
-@admin.register(SiteConfiguration)
-class SiteConfigurationAdmin(admin.ModelAdmin):
-    list_display = ("name", "value")    
-
 
 @admin.register(EquipmentType)
 class EquipmentTypeAdmin(admin.ModelAdmin):
@@ -24,9 +12,28 @@ class EquipmentTypeAdmin(admin.ModelAdmin):
     search_fields = ("name", "description", "slug")
     list_filter = ("name", "description", "slug")
 
+admin.site.register(License)
 
 @admin.register(MaintenanceRecordMenu)
 class MaintenanceRecordMenuAdmin(admin.ModelAdmin):
     list_display = ("equipment_type", "link_icon", "link_text")
     search_fields = ("equipment_type__name", "link_icon", "link_text")
     list_filter = ("equipment_type", "link_icon", "link_text")
+
+
+@admin.register(SiteConfiguration)
+class SiteConfigurationAdmin(admin.ModelAdmin):
+    list_display = ("name", "value")    
+
+admin.site.register(SoftwareType)
+
+@admin.register(RecordType)
+class RecordTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "color", "front_page")
+
+
+
+
+
+
+
